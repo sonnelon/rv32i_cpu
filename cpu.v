96da0e00a36433cda34f003b6730f339e8a606c2
sonnelon <sonnelon4@gmail.com>
2026-01-22 20:48:18 +0300

    add: i type instrs

--- cpu.v
+++ cpu.v
@@ -61,39 +61,6 @@ localparam TYPE_AUIPC = 2'h20;
 
 localparam NOTHING = 3'h100;
 
-localparam X0_OPCODE  = 5'b00000;
-localparam X1_OPCODE  = 5'b00001;
-localparam X2_OPCODE  = 5'b00010;
-localparam X3_OPCODE  = 5'b00011;
-localparam X4_OPCODE  = 5'b00100;
-localparam X5_OPCODE  = 5'b00101;
-localparam X6_OPCODE  = 5'b00110;
-localparam X7_OPCODE  = 5'b00111;
-localparam X8_OPCODE  = 5'b01000;
-localparam X9_OPCODE  = 5'b01001;
-localparam X10_OPCODE = 5'b01010;
-localparam X11_OPCODE = 5'b01011;
-localparam X12_OPCODE = 5'b01100;
-localparam X13_OPCODE = 5'b01101;
-localparam X14_OPCODE = 5'b01110;
-localparam X15_OPCODE = 5'b01111;
-localparam X16_OPCODE = 5'b10000;
-localparam X17_OPCODE = 5'b10001;
-localparam X18_OPCODE = 5'b10010;
-localparam X19_OPCODE = 5'b10011;
-localparam X20_OPCODE = 5'b10100;
-localparam X21_OPCODE = 5'b10101;
-localparam X22_OPCODE = 5'b10110;
-localparam X23_OPCODE = 5'b10111;
-localparam X24_OPCODE = 5'b11000;
-localparam X25_OPCODE = 5'b11001;
-localparam X26_OPCODE = 5'b11010;
-localparam X27_OPCODE = 5'b11011;
-localparam X28_OPCODE = 5'b11100;
-localparam X29_OPCODE = 5'b11101;
-localparam X30_OPCODE = 5'b11110;
-localparam X31_OPCODE = 5'b11111;
-
 parameter COUNT_RAM_WORD = 1024;
 parameter SIZE_WORD		 = 32;
 
@@ -197,52 +164,34 @@ always @ (posedge clk or posedge rst) begin
 		end
 
 		EXEC: begin
-			function get_reg [31:0] register;
-				input [31:0] num;
-				begin
-					case (num)
-						X0_OPCODE: begin
-						end
-						X1_OPCODE: begin
-						end
-						X2_OPCODE: begin
-						end
-						X3_OPCODE: begin
-						end
-						X4_OPCODE: begin
-						end
-						X5_OPCODE: begin
-						end
-						X6_OPCODE: begin
-						end
-						X7_OPCODE: begin
-						end
-						X8_OPCODE: begin
-						end
-						X9_OPCODE: begin
-						end
-						X10_OPCODE: begin
-						end
-						X11_OPCODE: begin
-						end
-					endcase
-				end
-			endfunction
 			case (current_instr_class)
 				TYPE_I: begin
 					case (funct3)
 						ADDI_FN3: begin
-							
+							regs[rd] <= regs[rs] + i_type_imm;					
 						end
 						SLTI_FN3: begin
+							if ($signed(regs[rs1]) < $signed(i_type_imm)) begin
+								regs[rd] <= 1;
+							end else begin
+								regs[rd] <= 0;
+							end
 						end
 						SLTIU_FN3: begin
+							if ($unsigned(regs[rs1]) < $unsigned(regs[rd])) begin
+								regs[rd] <= 1;
+							end else begin
+								regs[rd] <= 0;
+							end
 						end
 						XORI_FN3: begin
+							regs[rd] <= regs[rs1] ^ i_type_imm;
 						end
 						ORI_FN3: begin
+							regs[rd] <= regs[rs1] | i_type_imm;
 						end
 						ANDI_FN3: begin
+							regs[rd] <= regs[rs1] & i_type_imm;
 						end
 					endcase
 				end
